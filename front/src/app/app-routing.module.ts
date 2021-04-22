b258df891a2cb4d7903e2e85b70f81b7431befa3
lvishd <herzogdavid@hotmail.fr>
2021-04-22 13:01:54 +0200

    login (front)

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -6,14 +6,17 @@ import { HomeComponent } from './home/home.component';
 //import { HomeComponent } from './home/home.component';
 // import { ChartsModule } from 'ng2-charts';
 import { BrowserModule } from '@angular/platform-browser';
+import { LoginComponent } from './login/login.component';
 
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'detailsProduit', component: DetailsProductComponent },
   { path: 'manageStock', component: ManageStockComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
+
 ];
 
 @NgModule({
