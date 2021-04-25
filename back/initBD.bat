d1dbee9c91d91f6d7cfcb1a7d5ded651a3da14b5
Oleg <buklemishevoleg@gmail.com>
2021-04-25 18:31:30 +0200

    clean

--- back/initBD.bat
+++
@@ -1,6 +0,0 @@
-python manage.py migrate
-python manage.py refreshCoquillagesList
-python manage.py refreshCrustacesList
-python manage.py refreshPoissonsList
-python manage.py refreshProductList
-python manage.py refreshOnSaleList
